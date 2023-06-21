d866cca8d7c591d05f891eed314963fb55a40771
Meryem Yaren Küner <90837878+Meryrenk4@users.noreply.github.com>
2023-06-21 23:21:40 +0300

    Update ActivityByAirTemperature.java

--- src/ActivityByAirTemperature.java
+++ src/ActivityByAirTemperature.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
-// Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
-//Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
-//Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
-//Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+
 public class ActivityByAirTemperature {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
