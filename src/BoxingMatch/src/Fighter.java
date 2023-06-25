c04310a35109e396f4b08a056787b5e8d60fbb34
Meryrenk4 <90837878+Meryrenk4@users.noreply.github.com>
2023-06-25 13:43:51 +0300

    Boxing Match project is added

---
+++ src/BoxingMatch/src/Fighter.java
@@ -0,0 +1,55 @@
+/*
+    The system that calculates who will start the fight first with 50% probability
+    has been integrated into the project.
+
+    The whoStarts method, which selects which fighter to start, is added to this class.
+ */
+
+import java.util.Random;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    public int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " damaged " + this.damage + " to " +  foe.name + ".");
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " dodged incoming damage.");
+            return foe.health;
+        }
+
+        return Math.max((foe.health - this.damage), 0);
+
+    }
+
+    public boolean dodge() {
+        double randomValue = Math.random() * 100;  //0.0 to 99.9
+        return randomValue <= this.dodge;
+    }
+
+    // The method
+    public boolean whoStarts() {
+        // Create the random object
+        Random r = new Random();
+
+        // Module the random object by 2
+        // If result is 1, the first fighter will start
+        // If result is 0, the second fighter will start
+        int result = (r.nextInt(10-1) + 1) % 2;
+
+        return result == 1;
+    }
+}
---
+++ src/BoxingMatch/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Mark is the first fighter and Alex is the second fighter
+        Fighter marc = new Fighter("Marc" , 15 , 100, 100, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+
+        // Usage the method
+        if (marc.whoStarts()) {
+            // the condition is true, so the first fighter must be start
+            Ring r = new Ring(marc, alex, 100, 100);
+            r.run();
+        } else {
+            // the condition is false, so the second fighter must be start
+            Ring r = new Ring(alex, marc, 100, 100);
+            r.run();
+        }
+    }
+}
